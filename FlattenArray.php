eabc49324e4bb38ec51f5f1cc9b2812c6a90fa7b
Martin Roldan Araujo <rataciber@MacBook-Pro-de-Martin.local>
2017-05-19 16:33:33 -0400

    Revamp

--- FlattenArray.php
+++ FlattenArray.php
@@ -2,18 +2,38 @@
 namespace ratacibernetica;
 
 class FlattenArray {
-    
-    public static function flattenArray( array $unFlattened ) {
-            return array_reduce($unFlattened, function( $carry, $item2){
+
+    protected $array;
+
+    public function __construct($someArray){
+	$this->array = $someArray;
+    }    
+
+    public function __get($name){
+	switch($name){
+		case "flattened":
+			return $this->flattenArray($this->array);
+		break;
+		
+		default:
+			return null;
+		break;
+	}
+    }
+
+
+    public function flattenArray($unFlattenedArray=null ) {
+            if(!$unFlattenedArray){
+		$unFlattenedArray=$this->array;
+	    }
+            return array_reduce($unFlattenedArray, function( $carry, $item2){
                     if(!is_array($item2)){
                         $carry[]= $item2;
                     }
                     else{
-                        $carry = array_merge($carry, self::flattenArray($item2));
+                        $carry = array_merge($carry, $this->flattenArray($item2));
                     }
                     return $carry;
                 },[]);
         }
-        
-   
 }
--- tests/unit/FlattenArrayCest.php
+++ tests/unit/FlattenArrayCest.php
@@ -14,7 +14,8 @@ class FlattenArrayCest
     public function flattArrayTest(UnitTester $I)
     {
         $multipleArray = [ 1, [ 2 ], [[3],[[4]]]];
-        $flattenedArray = ratacibernetica\FlattenArray::flattenArray($multipleArray);
+        $flattener = new ratacibernetica\FlattenArray($multipleArray);
+	$flattenedArray = $flattener->flattened;
 
         $I->assertEquals( $flattenedArray, [1,2,3,4]);
     }
